36771a82b2309d8bace8174e95980acc40690b99
lek-tin <lek_tin@yahoo.com>
2018-06-26 15:04:59 +0800

    dev fix indentation;

--- gulpfile.js
+++ gulpfile.js
@@ -23,26 +23,26 @@ if ( env === 'production') {
 // Sass->CSS Task
 // Minify style.css if ENV === production
 gulp.task('sass', () => {
-  return gulp.src(sassEntry)
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    // For inline sourcemaps 
-    .pipe(sourcemaps.write())
-    // For file sourcemaps 
-    .pipe(sourcemaps.write('', {
-        includeContent: false,
-        sourceRoot: 'source'
-    }))
-    .pipe(gulp.dest(cssDest))
+    return gulp.src(sassEntry)
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        // For inline sourcemaps 
+        .pipe(sourcemaps.write())
+        // For file sourcemaps 
+        .pipe(sourcemaps.write('', {
+            includeContent: false,
+            sourceRoot: 'source'
+        }))
+        .pipe(gulp.dest(cssDest))
 })
 
 // Watch for Sass file changes
 gulp.task('watch', () => {
-  gulp.watch(sassSources, ['sass'])
+    gulp.watch(sassSources, ['sass'])
 })
 
 gulp.task('default', [
