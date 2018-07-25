afe5b8ec34a8b3e78024d5c92cf2b21f9fee1563
lek-tin <lek_tin@yahoo.com>
2018-07-26 01:42:50 +0800

    dev improve styles and interactivity of mobile slide-in menu;

---
+++ search.py
@@ -0,0 +1,23 @@
+nums = [1,3]
+target = 3
+def searchInsertPos(nums, target):
+  startPos, endPos = 0, len(nums) - 1
+
+  if target > nums[endPos]:
+    return len(nums)
+
+  if target < nums[0]:
+    return 0
+  
+  while True:
+    midPos = (startPos + endPos) // 2
+    if nums[midPos] == target:
+      return midPos
+    if nums[startPos] < target and nums[endPos] >target and (startPos + 1) == endPos:
+      return startPos + 1
+    if nums[midPos] < target:
+      startPos = midPos
+    if nums[midPos] > target:
+      endPos = midPos
+
+print(searchInsertPos(nums, target))
